6a5059281076c01030221f00acc597f8b0b0ccda
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-20 22:07:19 +0200

    feat: add Algoritm class

---
+++ src/main/java/algorithm/Algorithm.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import io.output.OutputForm;
+import lombok.experimental.UtilityClass;
+
+import java.util.Map;
+
+@UtilityClass
+public class Algorithm {
+    public OutputForm proceed() {
+        return null;
+    }
+
+    private float[] computeFactors() {
+        return null;
+    }
+}

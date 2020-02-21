81e9b41ab0844aac2ccec55d08813c286b895e6a
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-21 13:13:41 +0200

    feat: add algorithm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import algorithm.Algorithm;
 import io.input.InputManager;
+import io.output.Printer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print(InputManager.scan("res"));
+        Printer.print(Algorithm.proceed(InputManager.scan("f_libraries_of_the_world.txt")), "f_libraries_of_the_world.out");
     }
 }
--- src/main/java/algorithm/Algorithm.java
+++ src/main/java/algorithm/Algorithm.java
@@ -1,19 +1,42 @@
 package algorithm;
 
+import io.input.domain.Data;
+import io.input.domain.Library;
 import io.output.OutputForm;
 import lombok.experimental.UtilityClass;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @UtilityClass
 public class Algorithm {
-    public OutputForm proceed() {
-        return null;
+    public OutputForm proceed(Data data) {
+        Map<Integer, int[]> output = new HashMap<>();
+
+        int days = data.getDays();
+        int remainingDays;
+
+        int i = 0;
+        for (Library lib: data.getLibs()) {
+            remainingDays = days - lib.getSignUp();
+            int maxBooks = lib.getBooks().length;
+            int processBooks = remainingDays*lib.getMaxBooksPerDay();
+            if (maxBooks < processBooks) {
+                int[] indexesArray = new int[maxBooks];
+                for(int j = 0; j < maxBooks; j++) {
+                    indexesArray[j] = lib.getBooks()[j].getId();
+                }
+                output.put(i, indexesArray);
+            } else {
+                int[] indexesArray = new int[processBooks];
+                for(int j = 0; j < processBooks; j++) {
+                    indexesArray[j] = lib.getBooks()[j].getId();
+                }
+                output.put(i, indexesArray);
+            }
+            i++;
+        }
+        return new OutputForm(output);
     }
-
-    private float computeFactor(int concurrency, int[] array) {
-        return concurrency*3 + Arrays.stream(array).sum()/array.length;
-    }
-
-
 }
